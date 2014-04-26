baa79c0404268d8880fae7212f7857d36c832c8e
Jacinda Zhong <jacindaz@gmail.com>
2014-04-26 12:08:07 -0400

    Final Shuffle code
    Created my own shuffle methods, without realizing that Ruby had its own array.shuffle method!

--- shufflerubymonk.rb
+++ shufflerubymonk.rb
@@ -24,13 +24,13 @@ def firstNumSwappy(swap_num1, swap_index)
   str_swap_num1 = swap_num1.to_s
   first_num = str_swap_num1[0]
   swap_num = str_swap_num1[swap_index]
-  puts "1st \#: #{first_num}, 2nd\: #{swap_num}"
+  #puts "1st \#: #{first_num}, 2nd\: #{swap_num}"
 
   swapped_num = str_swap_num1
   swapped_num[0] = swap_num
   swapped_num[swap_index] = first_num
   swapped_num = swapped_num.to_i
-  puts "1st num swapped\: #{swapped_num}"
+  #puts "1st num swapped\: #{swapped_num}"
 
   return swapped_num
 end
@@ -53,9 +53,8 @@ def swapLastTwo(final_array)
   while iterate < 3
     final_array << innerSwappy(final_array[iterate])
     iterate += 1
-    puts "Final array: #{final_array}, iteration: #{iterate}"
+    #puts "Final array: #{final_array}, iteration: #{iterate}"
   end
-  return final_array
 end
 
 #-------------------CALCULATE FACTORIAL----------------------
@@ -66,4 +65,62 @@ def calcFactorial(num_to_fac)
     num_to_fac -= 1
   end
   return factorial
+end
+
+
+#-------------------COMBINING ALL SWAP METHODS----------------------
+def number_shuffle(number)
+  final_array = [number]
+  number = number.to_s
+  length = number.length
+
+  #swap the first digit in number-----------------------------------
+  while length > 1
+    swapped = firstNumSwappy(number, length - 1)
+    #puts "I swapped something: #{swapped}"
+    final_array << swapped
+    #puts "This is the array: #{final_array}, length: #{length}"
+    length -= 1
+  end
+
+  #puts "Final array: #{final_array}"
+
+  #swap inner digits dependant on length of #------------------------
+  if number.length === 3
+    swapLastTwo(final_array)
+    puts "Length 3: #{final_array}"
+    return final_array
+  elsif number.length == 4
+    while_iterate = 0
+    while while_iterate < 4
+      #puts "Length 4 while loop"
+      final_array << innerSwappy(final_array[while_iterate])
+      final_array << swappyByIndex(final_array[while_iterate], 1, 2)
+      final_array << innerSwappy(final_array[-1])
+      #puts "Swapped index 1,2: #{final_array}"
+      final_array << swappyByIndex(final_array[while_iterate], 1, 3)
+      final_array << innerSwappy(final_array[-1])
+      #puts "Swapped index 1,3: #{final_array}"
+      while_iterate += 1
+      #puts "Length 4 array: #{final_array}, iteration: #{while_iterate}, array-length: #{final_array.length}"
+    end
+    #puts "Length 4: #{final_array}"
+    return final_array
+  else
+    puts "Error!"
+  end
+
+end
+
+
+#-------------------FINAL METHOD WITH ALL SWAPS----------------------
+def checks(number)
+
+  #check to make sure have correct # of shuffled digits
+  if final_array.length == calcFactorial(number)
+    return final_array
+  elsif #are there repeats in the final array??
+    return final_array.uniq
+  else
+    puts "Sorry, that is not correct!"
 end
